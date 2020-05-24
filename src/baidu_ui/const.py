7f3eba80ab22ea3af8a367ee6c67d09ee529aaa1
gateaa-tea <gateaa@outlook.com>
2020-05-24 11:09:39 +0800

    all autotest file

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,3 @@
+# coding = utf -8
+
+EXCEL_DIR="E:\python\demo\data\query.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+BASE_URL = "http://httpbin.org/"
+IP_URL = "/ip"
+LOCAL_IP = "14.106.54.212"
+POST_TEST_URL = "/post"
